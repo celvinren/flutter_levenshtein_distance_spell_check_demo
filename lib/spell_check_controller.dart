623c51efbe5acafa0225c5a635ecf179065fe844
Calvin Ren <j.calvin.ren@gmail.com>
2024-11-09 21:55:12 +1000

    migrate to stream

--- lib/spell_check_controller.dart
+++ lib/spell_check_controller.dart
@@ -1,22 +1,33 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_levenshtein_distance_spell_check_demo/en.dart';
-import 'package:flutter_levenshtein_distance_spell_check_demo/es.dart';
 import 'package:flutter_levenshtein_distance_spell_check_demo/models/mistake.dart';
+import 'package:flutter_levenshtein_distance_spell_check_demo/spell_check_finder/spell_check_finder_mobile.dart';
+
+import 'en.dart';
+import 'es.dart';
 
 /// Spelling checker controller that extends TextEditingController
 class SpellCheckController extends TextEditingController {
   List<Mistake> mistakes = [];
   Timer? timer;
   Locale locale = const Locale('en');
-  final Set<String> checkedWords = {}; // Set to store checked words
+  Set<String> checkedWords = {}; // Set to store checked words
+  final SpellCheckFinder spellCheckFinder = SpellCheckFinder();
 
   SpellCheckController({this.locale = const Locale('en')})
       : assert(locale == const Locale('en') || locale == const Locale('es'),
-            'Only English and Spanish are supported');
+            'Only English and Spanish are supported') {
+    spellCheckFinder.stream.listen((event) {
+      mistakes = [
+        ...mistakes,
+        ...event.newMistakes.where((e) => !mistakes.contains(e)),
+      ]..sort((a, b) => a.offset.compareTo(b.offset));
+      checkedWords.addAll(event.newCheckedWords);
+      notifyListeners();
+    });
+  }
 
   /// Handles spelling check when text changes
   @override
@@ -25,7 +36,14 @@ class SpellCheckController extends TextEditingController {
 
     timer?.cancel();
     timer = Timer(const Duration(milliseconds: 600), () {
-      _checkSpelling(newValue.text);
+      if (text.isNotEmpty) {
+        spellCheckFinder.findMistakes(
+          text: text,
+          dictionary:
+              (locale.languageCode == 'en' ? enWords1 : esWords1).split('\n'),
+          checkedWords: checkedWords,
+        );
+      }
     });
   }
 
@@ -98,130 +116,11 @@ class SpellCheckController extends TextEditingController {
     return TextSpan(children: spans);
   }
 
-  /// Levenshtein distance algorithm
-  int _levenshteinDistance(String s, String t, {bool caseSensitive = false}) {
-    if (!caseSensitive) {
-      s = s.toLowerCase();
-      t = t.toLowerCase();
-    }
-    if (s == t) {
-      return 0;
-    }
-    if (s.isEmpty) {
-      return t.length;
-    }
-    if (t.isEmpty) {
-      return s.length;
-    }
-
-    List<int> v0 = List<int>.filled(t.length + 1, 0);
-    List<int> v1 = List<int>.filled(t.length + 1, 0);
-
-    for (int i = 0; i < t.length + 1; i++) {
-      v0[i] = i;
-    }
-
-    for (int i = 0; i < s.length; i++) {
-      v1[0] = i + 1;
-
-      for (int j = 0; j < t.length; j++) {
-        int cost = (s[i] == t[j]) ? 0 : 1;
-        v1[j + 1] = min(v1[j] + 1, min(v0[j + 1] + 1, v0[j] + cost));
-      }
-
-      for (int j = 0; j < t.length + 1; j++) {
-        v0[j] = v1[j];
-      }
-    }
-
-    return v1[t.length];
-  }
-
-  /// Checks spelling and generates suggestions
-  void _checkSpelling(String text) {
-    // Find new mistakes
-    mistakes = [
-      ...mistakes,
-      ..._findMistakes(text)
-          .map((e) => mistakes.contains(e) ? null : e)
-          .where((e) => e != null)
-          .cast<Mistake>(),
-    ]..sort((a, b) => a.offset.compareTo(b.offset));
-    notifyListeners();
-  }
-
-  List<Mistake> _findMistakes(String text) {
-    List<Mistake> results = [];
-
-    // Select dictionary based on language
-    final words = locale.languageCode == 'en' ? enWords1 : esWords1;
-    final dictionary = words.split('\n');
-
-    // Split the text into sentences based on punctuation
-    final sentenceRegex = RegExp(r'[^.!?]+');
-    final sentences = sentenceRegex.allMatches(text);
-
-    for (final sentenceMatch in sentences) {
-      final sentence = sentenceMatch.group(0)!;
-
-      // Use regular expression to match words in each sentence
-      final wordRegex = RegExp(r'\b\w+\b');
-      final wordMatches = wordRegex.allMatches(sentence).toList();
-
-      // Loop through each matched word within the sentence but skip the last one
-      for (int j = 0; j < wordMatches.length - 1; j++) {
-        final match = wordMatches[j];
-        final word = match.group(0)!;
-        final lowercasedWord = word.toLowerCase();
-
-        // Skip if the word has already been checked
-        if (checkedWords.contains(lowercasedWord)) {
-          continue;
-        }
-
-        // Perform spell check if word is not in dictionary
-        if (!dictionary.contains(lowercasedWord)) {
-          bool isCapitalized = word[0].toUpperCase() == word[0];
-
-          // Check if it might be a proper noun
-          bool isPotentialProperNoun = isCapitalized && j > 0;
-          // If it's the first word of a sentence, perform spell check, don't skip
-          if (isPotentialProperNoun) {
-            continue; // Skip the word if it's likely a proper noun in the middle of a sentence
-          }
-
-          // Calculate Levenshtein distance for all possible suggestions
-          List<MapEntry<String, int>> potentialSuggestions = dictionary
-              .map((entry) =>
-                  MapEntry(entry, _levenshteinDistance(entry, lowercasedWord)))
-              .where((entry) =>
-                  entry.value <= 2) // Filter words with distance <= 2
-              .toList();
-
-          // Sort by distance and select the top 10
-          potentialSuggestions.sort((a, b) => a.value - b.value);
-          List<String> suggestions = potentialSuggestions
-              .take(10)
-              .map<String>((entry) => isCapitalized
-                  ? (entry.key[0].toUpperCase() + entry.key.substring(1))
-                  : entry.key)
-              .toList();
-
-          // Add to the list of mistakes with correct offset in the original text
-          results.add(
-            Mistake(
-              offset: sentenceMatch.start + match.start,
-              length: word.length,
-              suggestions: suggestions,
-            ),
-          );
-        }
-
-        // Add the word to the checked words set
-        checkedWords.add(lowercasedWord);
-      }
-    }
-    return results;
+  @override
+  void dispose() {
+    timer?.cancel();
+    spellCheckFinder.dispose();
+    super.dispose();
   }
 
   /// Replaces the mistake with the selected suggestion
@@ -242,6 +141,7 @@ class SpellCheckController extends TextEditingController {
     // Calculate the difference in length between the mistake and the replacement
     int offsetDifference = replacement.length - mistake.length;
 
+    // Update the text value and selection
     value = TextEditingValue(
       text: newText,
       selection:
@@ -256,11 +156,14 @@ class SpellCheckController extends TextEditingController {
 
     // Add the replacement to the checked words set
     checkedWords.add(replacement.toLowerCase());
+    checkedWords.remove(text
+        .substring(mistake.offset, mistake.offset + mistake.length)
+        .toLowerCase());
     mistakes.remove(mistake);
     // notifyListeners();
   }
 
-  _showSuggestionsOverlay({
+  void _showSuggestionsOverlay({
     required BuildContext context,
     required List<PopupMenuEntry<VoidCallback>> items,
     GlobalKey? atWidget,
---
+++ lib/spell_check_finder/spell_check_finder_mobile.dart
@@ -0,0 +1,141 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter_levenshtein_distance_spell_check_demo/models/mistake.dart';
+
+typedef SpellCheckResult = ({
+  List<Mistake> newMistakes,
+  Set<String> newCheckedWords
+});
+
+/// Spell checker class that uses Levenshtein distance to find mistakes
+class SpellCheckFinder {
+  final StreamController<SpellCheckResult> _controller =
+      StreamController<SpellCheckResult>();
+
+  Stream<SpellCheckResult> get stream => _controller.stream;
+
+  void dispose() {
+    _controller.close();
+  }
+
+  void findMistakes({
+    required String text,
+    required List<String> dictionary,
+    required Set<String> checkedWords,
+  }) {
+    List<Mistake> newMistakes = [];
+    Set<String> newCheckedWords = {};
+
+    // Split the text into sentences based on punctuation
+    final sentenceRegex = RegExp(r'[^.!?]+');
+    final sentences = sentenceRegex.allMatches(text).toList();
+
+    for (int i = 0; i < sentences.length; i++) {
+      final sentence = sentences[i].group(0)!;
+
+      // Use regular expression to match words in each sentence
+      final wordRegex = RegExp(r'\b\w+\b');
+      final wordMatches = wordRegex.allMatches(sentence).toList();
+
+      // Loop through each matched word within the sentence but skip the last one
+      for (int j = 0;
+          j <
+              ((i == sentences.length - 1)
+                  ? wordMatches.length - 1
+                  : wordMatches.length);
+          j++) {
+        final match = wordMatches[j];
+        final word = match.group(0)!;
+        final lowercasedWord = word.toLowerCase();
+
+        // Skip if the word has already been checked
+        if (checkedWords.contains(lowercasedWord)) {
+          continue;
+        }
+
+        // Perform spell check if word is not in dictionary
+        if (!dictionary.contains(lowercasedWord)) {
+          bool isCapitalized = word[0].toUpperCase() == word[0];
+
+          // Check if it might be a proper noun
+          bool isPotentialProperNoun = isCapitalized && j > 0;
+          // If it's the first word of a sentence, perform spell check, don't skip
+          if (isPotentialProperNoun) {
+            continue; // Skip the word if it's likely a proper noun in the middle of a sentence
+          }
+
+          // Calculate Levenshtein distance for all possible suggestions
+          List<MapEntry<String, int>> potentialSuggestions = dictionary
+              .map((entry) =>
+                  MapEntry(entry, _levenshteinDistance(entry, lowercasedWord)))
+              .where((entry) =>
+                  entry.value <= 2) // Filter words with distance <= 2
+              .toList();
+
+          // Sort by distance and select the top 10
+          potentialSuggestions.sort((a, b) => a.value - b.value);
+          List<String> suggestions = potentialSuggestions
+              .take(10)
+              .map<String>((entry) => isCapitalized
+                  ? (entry.key[0].toUpperCase() + entry.key.substring(1))
+                  : entry.key)
+              .toList();
+
+          // Add to the list of mistakes with correct offset in the original text
+          newMistakes.add(
+            Mistake(
+              offset: sentences[i].start + match.start,
+              length: word.length,
+              suggestions: suggestions,
+            ),
+          );
+        }
+
+        // Add the word to the checked words set
+        newCheckedWords.add(lowercasedWord);
+      }
+    }
+    _controller
+        .add((newMistakes: newMistakes, newCheckedWords: newCheckedWords));
+  }
+
+  /// Levenshtein distance algorithm
+  int _levenshteinDistance(String s, String t, {bool caseSensitive = false}) {
+    if (!caseSensitive) {
+      s = s.toLowerCase();
+      t = t.toLowerCase();
+    }
+    if (s == t) {
+      return 0;
+    }
+    if (s.isEmpty) {
+      return t.length;
+    }
+    if (t.isEmpty) {
+      return s.length;
+    }
+
+    List<int> v0 = List<int>.filled(t.length + 1, 0);
+    List<int> v1 = List<int>.filled(t.length + 1, 0);
+
+    for (int i = 0; i < t.length + 1; i++) {
+      v0[i] = i;
+    }
+
+    for (int i = 0; i < s.length; i++) {
+      v1[0] = i + 1;
+
+      for (int j = 0; j < t.length; j++) {
+        int cost = (s[i] == t[j]) ? 0 : 1;
+        v1[j + 1] = min(v1[j] + 1, min(v0[j + 1] + 1, v0[j] + cost));
+      }
+
+      for (int j = 0; j < t.length + 1; j++) {
+        v0[j] = v1[j];
+      }
+    }
+
+    return v1[t.length];
+  }
+}
